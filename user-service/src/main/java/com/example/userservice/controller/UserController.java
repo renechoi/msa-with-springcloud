bb1ce6f52b46174c687d1e840a74a11cf4775aff
renechoi <115696395+renechoi@users.noreply.github.com>
2023-05-31 23:21:10 +0900

    feat: user-service 조회 기능 구현

--- user-service/src/main/java/com/example/userservice/controller/UserController.java
+++ user-service/src/main/java/com/example/userservice/controller/UserController.java
@@ -1,5 +1,8 @@
 package com.example.userservice.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,12 +11,14 @@ import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.userservice.dto.UserDto;
+import com.example.userservice.jpa.UserEntity;
 import com.example.userservice.service.UserService;
 import com.example.userservice.vo.Greeting;
 import com.example.userservice.vo.RequestUser;
@@ -22,7 +27,7 @@ import com.example.userservice.vo.ResponseUser;
 import lombok.RequiredArgsConstructor;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/user-service")
 @RequiredArgsConstructor
 public class UserController {
 
@@ -31,7 +36,7 @@ public class UserController {
 	private final UserService userService;
 
 
-	@GetMapping("user-service/health_check")
+	@GetMapping("/health_check")
 	public String status() {
 		return String.format("It's Working in User Service"
 			+ ", port(local.server.port)=" + env.getProperty("local.server.port")
@@ -56,5 +61,24 @@ public class UserController {
 		ResponseUser responseUser = mapper.map(userDto, ResponseUser.class);
 
 		return ResponseEntity.status(HttpStatus.CREATED).body(responseUser);
+	}
+
+	@GetMapping("/users")
+	public ResponseEntity<List<ResponseUser>> getUsers() {
+		Iterable<UserEntity> userList = userService.getUserByAll();
+
+		List<ResponseUser> result = new ArrayList<>();
+		userList.forEach(v -> result.add(new ModelMapper().map(v, ResponseUser.class)));
+
+		return ResponseEntity.status(HttpStatus.OK).body(result);
+	}
+
+	@GetMapping("/users/{userId}")
+	public ResponseEntity<ResponseUser> getUser(@PathVariable("userId") String userId) {
+		UserDto userDto = userService.getUserByUserId(userId);
+
+		ResponseUser returnValue = new ModelMapper().map(userDto, ResponseUser.class);
+
+		return ResponseEntity.status(HttpStatus.OK).body(returnValue);
 	}
 }
--- user-service/src/main/java/com/example/userservice/service/UserServiceImpl.java
+++ user-service/src/main/java/com/example/userservice/service/UserServiceImpl.java
@@ -1,9 +1,13 @@
 package com.example.userservice.service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.cloud.client.circuitbreaker.CircuitBreaker;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -11,10 +15,13 @@ import org.springframework.stereotype.Service;
 import com.example.userservice.dto.UserDto;
 import com.example.userservice.jpa.UserEntity;
 import com.example.userservice.jpa.UserRepository;
+import com.example.userservice.vo.ResponseOrder;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Service
+@Slf4j
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService{
 
@@ -33,5 +40,20 @@ public class UserServiceImpl implements UserService{
 		userRepository.save(userEntity);
 
 		return mapper.map(userEntity, UserDto.class);
+	}
+
+	@Override
+	public UserDto getUserByUserId(String userId) {
+		UserEntity userEntity = userRepository.findByUserId(userId);
+
+		if (userEntity == null)
+			throw new UsernameNotFoundException("User not found");
+
+		return new ModelMapper().map(userEntity, UserDto.class);
+	}
+
+	@Override
+	public Iterable<UserEntity> getUserByAll() {
+		return userRepository.findAll();
 	}
 }
